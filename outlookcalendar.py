470eaf1aef100b96feb46e0b71fec96c7b769eee
Alok Singh <singhalok602@gmail.com>
2023-05-13 01:28:24 -0700

    Put the code implementation under the main() to use the classes in other files

--- outlookcalendar.py
+++ outlookcalendar.py
@@ -10,26 +10,9 @@ save it in a file where it can be read and fill in the calendar
 
 """
 
-desc = """Give the name of the entry/activity as the first param and the date and time ([Month] [Date], [Year] from [start time] am/pm to [end time] am/pm)"""
-
-
-try:
-    subject = sys.argv[1]
-    date_and_time = sys.argv[2]
-except:
-    print("arguments missing")
-    print(desc)
-    exit(-1)
-
-
-
-
-username = "aloksingh2012@outlook.com"
-password = getpass.getpass()
-
 
 class DateTimeStringParser():
-    def parse_datetime_range(self, date_and_time: str):
+    def parse_datetime_range(self, date_and_time: str, desc: str):
         tz = EWSTimeZone.localzone()
         datetime_range_regex = r'^(\w{3})\s+(\d{1,2}),\s+(\d{4})\s+from\s+(\d{1,2}):(\d{2})\s+(am|pm)\s+to\s+(\d{1,2}):(\d{2})\s+(am|pm)$'
         match = re.match(datetime_range_regex, date_and_time, re.IGNORECASE)
@@ -79,8 +62,10 @@ class DateTimeStringParser():
         return months.get(month_abbr.lower())
 
 class MakeSchedule():
-    def __init__(self) -> None:
-        self.credentials = Credentials(username=username, password=password)
+    def __init__(self, username, password) -> None:
+        self.username = username
+        self.password = password
+        self.credentials = Credentials(username=self.username, password=self.password)
         self.account = Account(primary_smtp_address=username, credentials=self.credentials, autodiscover=True, access_type=DELEGATE)
 
     def make_item(self, subject: str, start_time, end_time, body=None, location=None):
@@ -101,11 +86,30 @@ class MakeSchedule():
 
 
 def main():
+
+    desc = """Give the name of the entry/activity as the first param and the date and time ([Month] [Date], [Year] from [start time] am/pm to [end time] am/pm)"""
+
+
+    try:
+        subject = sys.argv[1]
+        date_and_time = sys.argv[2]
+    except:
+        print("arguments missing")
+        print(desc)
+        exit(-1)
+
+
+
+
+    username = "aloksingh2012@outlook.com"
+    password = getpass.getpass()
+
+
     dtsp = DateTimeStringParser()
 
-    makesch = MakeSchedule()
+    makesch = MakeSchedule(username, password)
 
-    start_time, end_time = dtsp.parse_datetime_range(date_and_time)
+    start_time, end_time = dtsp.parse_datetime_range(date_and_time, desc)
 
     makesch.make_item(subject, start_time, end_time)
 
